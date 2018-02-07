31c48d9e70376c1149eb3259d47bdb088ba7590b
Cole Burns <burns177@ad.nmc.edu>
2018-02-07 15:17:31 -0500

    Add project files.

---
+++ IODemo/VideoGame.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IODemo
+{
+    public class VideoGame
+    {
+        public enum PlatformName
+        {
+            NONE,
+            XBOX,
+            PLAYSTATION
+        }
+
+        private int _id;
+        private string _name;
+        private PlatformName _platform;
+
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public PlatformName Platform
+        {
+            get { return _platform; }
+            set { _platform = value; }
+        }
+
+    }
+}
---
+++ IoDemoXML/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace IODemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VideoGame videoGame;
+            videoGame = InitializeVideoGame();
+            WriteXmlFile(videoGame);
+        }
+
+        public static VideoGame InitializeVideoGame()
+        {
+            VideoGame videoGame = new VideoGame()
+            {
+                Id = 1,
+                Name = "Mario",
+                Platform = VideoGame.PlatformName.XBOX
+            };
+
+            return videoGame;
+        }
+
+        public static void WriteXmlFile(VideoGame videoGame)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(VideoGame));
+            {
+                StreamWriter sWriter = new StreamWriter("Data.xml");
+
+                using (sWriter)
+                {
+                    serializer.Serialize(sWriter, videoGame);
+                }
+            };
+        }
+
+        public static VideoGame ReadXmlFile()
+        {
+            VideoGame videoGame;
+
+            StreamReader sReader = new StreamReader("Data.xml");
+
+            XmlSerializer serializer = new XmlSerializer(typeof(VideoGame));
+
+            using (sReader)
+            {
+                object xmlObject = serializer.Deserialize(sReader);
+                videoGame = xmlObject as VideoGame;
+
+                videoGame = serializer.Deserialize(sReader) as VideoGame;
+            }
+
+            return videoGame;
+        }
+    }
+}
